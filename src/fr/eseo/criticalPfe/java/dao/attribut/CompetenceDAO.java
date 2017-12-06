6f373362c5cc368e1202f1c09f07bc94ba094d5f
Clément Begnaud <clement.begnaud@gmail.com>
2017-12-06 13:52:12 +0100

    [MOD] Mise en interface : CompetenceDAO

--- src/fr/eseo/criticalPfe/java/dao/attribut/CompetenceDAO.java
+++ src/fr/eseo/criticalPfe/java/dao/attribut/CompetenceDAO.java
@@ -6,35 +6,49 @@ import java.sql.SQLException;
 import fr.eseo.criticalPfe.java.dao.DAO;
 import fr.eseo.criticalPfe.java.model.attributs.Competence;
 
-public class CompetenceDAO extends DAO<Competence>{
-
+public interface CompetenceDAO extends DAO<Competence>{
+    
+    /**
+     * Insère une compétence en BDD selon les paramètres donnés.
+     * @param nomCompetence le nom de la compétence
+     * @param nomCaracteristique le nom de la caractéristique de la compétence
+     * @return la compétence insérée le cas échéant
+     */
+    public Competence creer(String nomCompetence, String nomCaracteristique, String formationNecessaire, String competenceDeClasse, String malusArmure , String niveauDeCompetence, String description);
+    
+    public Competence creer(Competence comp);
+    
+    /**
+     * Met à jour une Competence en BDD (recherche par nom)
+     * @param nomCompetence le nom de la compétence
+     * @param nomCaracteristique le nom de la caractéristique de la compétence
+     * @return la compétence à mettre à jour le cas échéant
+     */
+    public Competence modifier(String nomCompetence, String nomCaracteristique, String formationNecessaire, String competenceDeClasse, String malusArmure , String niveauDeCompetence, String description);
+    
+    public Competence modifier(Competence comp);
+    
+    /**
+     * Récupère une Compétence en BDD (recherche par nom)
+     * @param nomCompetence le nom de la compétence
+     * @return la compétence à récupérer le cas échéant
+     */
+    public Competence trouver(String nomCompetence);
+    
+    public Competence trouver(Competence comp);
+    
+    /**
+     * Supprime une Compétence en BDD (recherche par nom)
+     * @param nomCompetence le nom de la compétence
+     * @return la compétence à supprimer, le cas échéant
+     */
+    public Competence supprimer(String nomCompetence);
+    
+    /**
+     * Transforme un ResultSet en Competence.
+     * @param result le resultSet à transformer
+     * @return la compétence à récupérer le cas échéant.
+     */
 	@Override
-	public Competence creer(Competence obj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean supprimer(Competence obj) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Competence trouver(Competence obj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Competence modifier(Competence obj) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Competence map(ResultSet result) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Competence map(ResultSet result) throws SQLException;
 }
