4810deab23ff5291ec426e7691a33eb00e293476
Amaryill <Amaryill@DESKTOP-UVI5JCE.home>
2017-12-07 11:03:07 +0100

    Creation de ModifPresentation
    On peut maintenant changer sa présentation à partir de son profil

--- WebContent/site/Profil/profil.jsp
+++ WebContent/site/Profil/profil.jsp
@@ -37,6 +37,42 @@
 	      <!--main content start-->
 	      <section id="main-content">
 	          <section class="wrapper">
+	          	<br><br>
+	          	<div class="panel panel-default">
+					<div class="panel-heading">
+						<h2 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion"
+								href="#collapse1">Présentation</a>
+						</h2>
+					</div>
+					
+					<% if (request.getSession().getAttribute("presentation") != null ){
+							out.println("<h5>"+request.getSession().getAttribute("presentation")+"</h5>");
+					}
+					%>
+	          
+					<br>
+					
+					
+					
+					<div id="collapse1" class="panel-collapse collapse in"
+						style="margin-top: 10px; margin-bottom: 10px">
+			          	<form class="form-horizontal" action="../../ModifPresentation" method="post">
+				          	<div class="form-group">
+				          		<div class="col-xs-12 col-xs-offset-0">
+					          		<textarea class="form-control" rows="2" id="presentation" placeholder="Entrez votre présentation" name="presentation"></textarea>
+				          		</div>
+				          	</div>
+				          	<div class="form-group">
+								<div class="col-xs-4 col-xs-offset-0">
+									<button type="submit" class="btn btn-default">Modifier</button>
+								</div>
+							</div>	
+			          	</form>
+			        </div>
+		        </div>
+	          
+	          	
 	          	
 	          </section>
 	      </section>
---
+++ src/fr/eseo/criticalPfe/servlets/ModifPresentation.java
@@ -0,0 +1,62 @@
+package fr.eseo.criticalPfe.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eseo.criticalPfe.bdd.BddBo;
+
+/**
+ * Servlet implementation class ModifPresentation
+ */
+@WebServlet("/ModifPresentation")
+public class ModifPresentation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static final String ATT_USER         = "utilisateur";
+    public static final String ATT_FORM         = "form";
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+    public static final String ATT_PRES         = "presentation";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModifPresentation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		BddBo bddBo = new BddBo();
+		HttpSession session = request.getSession();
+		
+		String presentation = (String) request.getParameter("presentation");
+		String pseudo_session = (String)request.getSession().getAttribute("utilisateur");
+		
+		bddBo.modifPres(pseudo_session, presentation);
+		
+		session.setAttribute(ATT_PRES, presentation);
+		String presentationSession = (String)request.getSession().getAttribute("presentation");
+		response.sendRedirect("/Critical-PFE/site/Profil/profil.jsp");
+		
+		
+		doGet(request, response);
+	}
+
+}
