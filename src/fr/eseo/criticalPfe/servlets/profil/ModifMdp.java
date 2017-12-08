e9556311dadf5e152b3ac94bef8758c525263e48
Amaryill <Amaryill@DESKTOP-UVI5JCE.home>
2017-12-08 09:09:57 +0100

    Adaptation modification MDP au nouveau formalisme

--- src/fr/eseo/criticalPfe/servlets/profil/ModifMdp.java
+++ src/fr/eseo/criticalPfe/servlets/profil/ModifMdp.java
@@ -15,6 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import fr.eseo.criticalPfe.bdd.BddBo;
+import fr.eseo.criticalPfe.java.bo.utilisateur.UtilisateurBOImpl;
+import fr.eseo.criticalPfe.java.dao.utilisateur.UtilisateurDAO;
+import fr.eseo.criticalPfe.java.model.utilisateur.Utilisateur;
 
 /**
  * Servlet implementation class ModifMdp
@@ -39,46 +42,77 @@ public class ModifMdp extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		BddBo bddBo = new BddBo();
-		Boolean erreur = false;
-		String msgErreur = "";
-
-		HttpSession session = request.getSession();	
-
-
-		/* Ici, nous placerons nos requ�tes vers la BDD */
-		/* Cr�ation de l'objet g�rant les requ�tes */
+	
 		String mdp_ancien = (String) request.getParameter("mdp_ancien");
 		String mdp_nouveau = (String) request.getParameter("mdp_nouveau");
 		String confirmer =(String) request.getParameter("confirmer");
-		String utilisateur1 =(String)request.getSession().getAttribute("utilisateur");
+		String utilisateurLogin =(String)request.getSession().getAttribute("utilisateur");
 		
-		String mdpBdd="";
-		try {
-			mdpBdd = bddBo.getMdp(utilisateur1);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
-
-		//Requete sql
-		if (confirmer.contentEquals(mdp_ancien) && mdp_ancien.contentEquals(mdpBdd)){
-			bddBo.modifMdp(utilisateur1, mdp_nouveau);
+		UtilisateurBOImpl boUtilisateur = null;
+		Utilisateur utilisateur = null;
+		
+		boUtilisateur = new UtilisateurBOImpl(new UtilisateurDAO());
+		utilisateur = new Utilisateur();
+		utilisateur.setPassword(mdp_ancien);
+		utilisateur.setLogin(utilisateurLogin);
+		
+		utilisateur = boUtilisateur.pullUtilisateur(utilisateur);
+		
+		if (confirmer.contentEquals(mdp_ancien) && mdp_ancien.contentEquals(utilisateur.getPassword())){
+			utilisateur.setPassword(mdp_nouveau);
+			boUtilisateur.modifUtilisateur(utilisateur);
+			
 			response.sendRedirect("/Critical-PFE/site/options.jsp");
 		} else {
 			response.sendRedirect("/Critical-PFE/site/options.jsp");
 		}
-
-
-
-		doGet(request, response);
+	
 	}
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		BddBo bddBo = new BddBo();
+//		Boolean erreur = false;
+//		String msgErreur = "";
+//
+//		HttpSession session = request.getSession();	
+//
+//
+//		/* Ici, nous placerons nos requ�tes vers la BDD */
+//		/* Cr�ation de l'objet g�rant les requ�tes */
+//		String mdp_ancien = (String) request.getParameter("mdp_ancien");
+//		String mdp_nouveau = (String) request.getParameter("mdp_nouveau");
+//		String confirmer =(String) request.getParameter("confirmer");
+//		String utilisateur1 =(String)request.getSession().getAttribute("utilisateur");
+//		
+//		String mdpBdd="";
+//		try {
+//			mdpBdd = bddBo.getMdp(utilisateur1);
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//
+//
+//		//Requete sql
+//		if (confirmer.contentEquals(mdp_ancien) && mdp_ancien.contentEquals(mdpBdd)){
+//			bddBo.modifMdp(utilisateur1, mdp_nouveau);
+//			response.sendRedirect("/Critical-PFE/site/options.jsp");
+//		} else {
+//			response.sendRedirect("/Critical-PFE/site/options.jsp");
+//		}
+//
+//
+//
+//		doGet(request, response);
+//	}
 
 }
